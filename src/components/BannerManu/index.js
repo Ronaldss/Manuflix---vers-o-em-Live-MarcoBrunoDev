123a4ad919ba84c2d483aa5270a366c8e32bc1ca
Ronald Santos <ronaldcontact2019@gmail.com>
2020-10-18 02:01:09 -0300

    :building_construction: using containers for banner styling and others

--- src/components/BannerManu/index.js
+++ src/components/BannerManu/index.js
@@ -1,10 +1,9 @@
 import styled from "styled-components";
+import container from "../../tools/container";
 
 const BannerManu = styled.section`
   background: orange;
-  max-width: 1360px;
-  margin-left: auto;
-  margin-right: auto;
+  ${container}
 `;
 
 export default BannerManu;
---
+++ src/tools/container.js
@@ -0,0 +1,7 @@
+const container = `
+max-width: 1360px;
+margin-left: auto;
+margin-right: auto;
+`;
+
+export default container;
