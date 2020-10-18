a5f88eb5beebaba1bcfb3b660ca2d4cf7283974b
Ronald Santos <ronaldcontact2019@gmail.com>
2020-10-18 01:52:08 -0300

    correction of banner width

--- src/components/BannerManu/index.js
+++ src/components/BannerManu/index.js
@@ -2,6 +2,9 @@ import styled from "styled-components";
 
 const BannerManu = styled.section`
   background: orange;
+  max-width: 1360px;
+  margin-left: auto;
+  margin-right: auto;
 `;
 
 export default BannerManu;
