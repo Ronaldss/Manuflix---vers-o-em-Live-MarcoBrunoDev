5eb3a980ef6fa730842ac3afc4803c9ac26e9bd0
Ronald Santos <ronaldcontact2019@gmail.com>
2020-10-29 21:29:11 -0300

    Create BodyManu component

--- src/App.js
+++ src/App.js
@@ -13,47 +13,52 @@ import DescriptionManu from "./components/DescriptionManu/index.js";
 import ThumbManu from "./components/ThumbManu/index.js";
 import dev from "./assests/img/dev.png";
 import avatarImg from "./assests/img/avatar.jpg";
+import BodyManu from "./components/BodyManu/index.js";
 
 function App() {
   return (
     <>
-      <HeaderManu>
-        <Wrapper>
-          <LogoManu />
-          <ButtonManu>Novo Vídeo</ButtonManu>
-        </Wrapper>
-      </HeaderManu>
-      <BannerManu>
-        <Text>
-          <TagManu>Tudo de História</TagManu>
-          <TitleManu>Os Hebreus e o monoteismo judáico</TitleManu>
-          <DescriptionManu>
-            Os antigos hebreus foram um povo semítico da região do Levante,
-            localizado no Oriente Médio. O etnônimo também foi utilizado a
-            partir do período romano para se referir aos judeus, um grupo étnico
-            e religioso de ascendência hebraica.
-          </DescriptionManu>
-        </Text>
+      <BodyManu>
+        <HeaderManu>
+          <Wrapper>
+            <LogoManu />
+            <ButtonManu as="a" href="/video/new">
+              Novo Vídeo
+            </ButtonManu>
+          </Wrapper>
+        </HeaderManu>
+        <BannerManu>
+          <Text>
+            <TagManu>Tudo de História</TagManu>
+            <TitleManu>Os Hebreus e o monoteismo judáico</TitleManu>
+            <DescriptionManu>
+              Os antigos hebreus foram um povo semítico da região do Levante,
+              localizado no Oriente Médio. O etnônimo também foi utilizado a
+              partir do período romano para se referir aos judeus, um grupo
+              étnico e religioso de ascendência hebraica.
+            </DescriptionManu>
+          </Text>
 
-        <ThumbManu
-          src={dev}
-          alt="Thumb da minha tela"
-          avatar={avatarImg}
-          channelName="Tudo de História"
-          title="Os Hebreus e o monoteismo judáico"
-          time="00:02:00"
-        />
-      </BannerManu>
-      <FooterMana>
-        <LogoManu />
-        <p>
-          Site feito na <HighlightManu>#ImersãoReact</HighlightManu> da{" "}
-          <LinkManu href="https://alura.com.br">Alura.</LinkManu>
-          <a href="https://github.com/Ronaldss/Manuflix---vers-o-em-Live-MarcoBrunoDev">
-            <LogoImersao />
-          </a>
-        </p>
-      </FooterMana>
+          <ThumbManu
+            src={dev}
+            alt="Thumb da minha tela"
+            avatar={avatarImg}
+            channelName="Tudo de História"
+            title="Os Hebreus e o monoteismo judáico"
+            time="00:02:00"
+          />
+        </BannerManu>
+        <FooterMana>
+          <LogoManu />
+          <p>
+            Site feito na <HighlightManu>#ImersãoReact</HighlightManu> da{" "}
+            <LinkManu href="https://alura.com.br">Alura.</LinkManu>
+            <a href="https://github.com/Ronaldss/Manuflix---vers-o-em-Live-MarcoBrunoDev">
+              <LogoImersao />
+            </a>
+          </p>
+        </FooterMana>
+      </BodyManu>
     </>
   );
 }
---
+++ src/components/BodyManu/index.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const BodyManu = styled.div`
+  width: 80%;
+`;
+
+export default BodyManu;
