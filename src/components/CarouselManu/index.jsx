da4d4798815e448ddeadbc94512a8a8f1bad1635
Ronald Santos <ronaldcontact2019@gmail.com>
2020-12-03 22:41:27 -0300

    Primeiros vídeos do carousel mostrados em coluna

--- src/components/CarouselManu/index.jsx
+++ src/components/CarouselManu/index.jsx
@@ -2,33 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ThumbManu from '../ThumbManu';
 
-function CarouselManu({videos}) {
-    const {src, alt, title, avatar, channelName, time, link} = videos[0];
-    return (
+function CarouselManu({ videos }) {
+  return (
+    <div>
+      { videos.map(({
+        src, alt, title, avatar, channelName, time, link,
+      }) => (
         <ThumbManu
-            src={src}
-            alt={alt}
-            title={title}
-            avatar={avatar}
-            channelName={channelName}
-            time={time}
-            link={link}
+          src={src}
+          alt={alt}
+          title={title}
+          avatar={avatar}
+          channelName={channelName}
+          time={time}
+          link={link}
         />
-    ); 
+      ))}
+    </div>
+  );
 }
 
 const typeVideos = {
-    src: PropTypes.string.isRequired,
-    alt: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    avatar: PropTypes.string.isRequired,
-    channelName: PropTypes.string.isRequired,
-    time: PropTypes.string.isRequired,
-    link: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  channelName: PropTypes.string.isRequired,
+  time: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
 };
-
+console.log(typeVideos);
 CarouselManu.propTypes = {
-    videos: PropTypes.arrayOf.isRequired,
+  videos: PropTypes.arrayOf.isRequired,
 };
 
-export  default CarouselManu; 
+export default CarouselManu;
